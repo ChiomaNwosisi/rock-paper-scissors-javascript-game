577c920bf0b73d9744e86a8fac8c79c8870efb69
Chioma Nwosisi <nwosisic5@gmail.com>
2022-10-14 23:26:40 +0100

    javascript game

---
+++ main.js
@@ -0,0 +1,59 @@
+let playerScore = 0;
+let compScore = 0;
+
+function computerplay(){
+const computerChoices = ['rock', 'paper', 'scissors'];
+const choices = Math.floor([Math.random()*computerChoices.length])
+return computerChoices[choices]
+};
+
+// Game Round
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection===computerSelection){
+    return `You have both picked ${playerSelection}! You are tied`
+  }
+  // Scissors
+  else if (playerSelection==='scissors' && computerSelection === 'rock'){
+    compScore++
+    return 'You Lose! Rock beats Scissors'
+  }else if (playerSelection==='scissors' && computerSelection === 'paper'){
+    playerScore++
+    return 'You Win! Scissors cuts Paper'
+  }
+  // Rock
+  else if(playerSelection==='rock' && computerSelection === 'paper'){
+    compScore++
+    return 'You Lose! Paper covers Rock'
+  }else if(playerSelection==='rock' && computerSelection === 'scissors'){
+    playerScore++
+    return 'You Win! Rock beats Scissors'
+  }
+// Paper
+  else if(playerSelection==='paper' && computerSelection === 'scissors'){
+    compScore++
+    return 'You Lose! Scissors cuts Paper'
+  }else if(playerSelection==='paper' && computerSelection === 'rock'){
+    playerScore++
+    return 'You Win! Paper covers rock'
+  }
+}
+
+// Game
+function game() {
+  for (i=0; i<5; i++){
+    playerSelection = prompt('Choose Your Weapon:\n Rock, Paper, Scissors\n Play 5 rounds to beat the computer').toLowerCase()
+  computerSelection = computerplay()
+  console.log("1", playRound(playerSelection, computerSelection));
+
+  } 
+
+  if (playerScore > compScore){
+    return "You WON the game Congratulations!"
+  }else if (playerScore < compScore){
+    return "You LOST the game better luck next time"
+  }else{
+    return "You TIED eith the computer not bad"
+  }
+}
+
+console.log(game()); 
